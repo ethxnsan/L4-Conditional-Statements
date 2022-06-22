e3ec9bda8a764833029c5ebe9410f3a7d5dba62f
Ethan Lew <107879719+ethxnsan@users.noreply.github.com>
2022-06-22 16:48:43 +0000

    months and quadrant values

--- README.md
+++ README.md
@@ -12,4 +12,24 @@ else:
   BODY STATEMENT
 ```
  python relies on indentation to define scope
- 
+
+ ### Elif
+
+ `elif allows us to add more than one condition to our `if` blocks
+
+ Conditional statements always start with an `if` header, `elif` is optional, but we can have as many as we want. `else` is also optional, but we can only have 1 `else` for every `if` 
+
+ Ex.
+```python
+month = 2
+if(month == 1):
+  print("January")
+elif (month == 2)
+  print("February")
+elif (month == 3)
+  print("March")
+elif (month == 4)
+  print("April")
+else:
+  print("The month is between May and December")  
+```
--- main.py
+++ main.py
@@ -11,8 +11,9 @@ else:
   print("Do nothing")
 
 userNumber = int(input("Give me a number: "))
-if(userNumber>0):
-  print("Your numberis positive")
+
+if(userNumber > 0):
+  print("Your number is positive")
 else:
   print("Your number is negative")
 
@@ -21,11 +22,28 @@ else:
   #if they are younger than 17 but older than 13, let them know they can watch G-rated movies and PG-13
   #if they're younger than 13, they're only allowed to watch just G-rated movies
 
-  userAge= int(input("Enter your age:"))
+userAge = int(input("Enter your age:"))
 
 if(userAge >=17): 
   print("You can watch all movies!")
-elif(userAge <17 and >=13):
+elif(userAge <17 and 13):
   print("You can watch G-Rated and PG-13 movies!")
 else:
   print("You can only watch G-Rated movies.")
+
+# Ask the user for an x and a y value\
+# Based on x and y value, output which quadrant they're in
+
+x= int(input("Enter your X value:"))
+y= int(input("Enter your Y value:"))
+
+if(x > 0 and y > 0):
+  print("quadrant 1")
+elif (x < 0 and y < 0):
+  print("quadrant 4")
+elif (x > 0 and y <0):
+  print("quadrant 4")
+elif (userX < 0):
+  print("quadrants 2 or 3")
+  
+  
